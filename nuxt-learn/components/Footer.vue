5c74d9befb872e3074ca27f67087a7ef2ce074c6
chenglin <837460387@qq.com>
2018-12-23 10:49:23 +0800

    nuxt init

---
+++ nuxt-learn/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    Visit our website for more documentation111 : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+  </footer>
+</template>
---
+++ nuxt-learn/pages/search.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    Page is search
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'search'
+}
+</script>
+
+<style lang="css">
+</style>
